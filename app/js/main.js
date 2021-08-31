b467f9bd33b2f2c9e501e71c81b71c0bb915fa03
Denis <example@gmail.com>
2021-08-31 19:03:23 +0300

    add footer section

--- app/js/main.js
+++ app/js/main.js
@@ -109,3 +109,20 @@ resetBtn.addEventListener('click', () => {
   input.value = ''
   
 })
+
+/* Scroll to top*/
+$( window).on('scroll',function() {
+  let offtop = $(this).scrollTop();
+  let page_h = document.body.clientHeight;
+  if (offtop > 580){$('.totop').fadeIn('fast');}
+  else{$('.totop').fadeOut('fast');}
+  if(offtop > page_h - 900){
+      $('.totop').addClass('stopped')
+  }
+  else{
+      $('.totop').removeClass('stopped')
+  }
+});
+$('.totop').on('click',function(){
+  $('html, body').animate({ scrollTop: 0 }, 500);
+});
